5d6aa8176df65900e69b3b59746e9b843bcf3117
GoodluckH <lixipu98@gmail.com>
2017-11-26 02:38:51 -0800

    Update R regression.R

--- R regression.R
+++ R regression.R
@@ -26,8 +26,10 @@ colnames(monthly.risk.premium) <- colnames(price)
 index.model.APPLE <- lm(APPLE~SP500, data = monthly.risk.premium)
 
 # Visualise the data using ggplot2 
-p <- (
+p<-(
 (ggplot(monthly.risk.premium, aes(SP500, APPLE))+
+   geom_segment(aes(x=0,xend=0,y=-Inf,yend=0.1975),color='#e5001f',lwd=0.4)+
+   geom_segment(aes(x=0,xend=0,y=-0.1975,yend=0.1975),color='#e5001f',lwd=1.1)+
   geom_point(col='#014d64')+
   labs(title = 'A monolithic bite.',
        subtitle= "Apple's beta is less than 1.\nRisk premium regression model.",
@@ -38,17 +40,15 @@ p <- (
   geom_abline(intercept=index.model.APPLE$coefficients[1], slope=index.model.APPLE$coefficients[2],
               lty=2,lwd=1.2,color='#56B4E9', xlim(-0.2,0.1))
     )+
-theme(plot.title = element_text(family = 'Avenir Next Condensed',size=18,margin=margin(-7,0,10,0)),
+theme(plot.title = element_text(family = 'Avenir Next Condensed', hjust=0,size=18,margin=margin(-7,0,10,0)),
     plot.subtitle = element_text(family = 'Avenir Next Condensed',size=12, margin=margin(-2,0,3,0)),
     plot.caption = element_text(family = 'Avenir Next Condensed', size=9, hjust = 0,margin=margin(34,0,-45,0)),  
     axis.text = element_text(face='bold'),
     axis.title.x = element_text(family = 'Avenir Next Condensed',face = 'bold', margin=margin(12,0,-12,0)),
     axis.title.y = element_text(family = 'Avenir Next Condensed',face = 'bold', margin=margin(0,12,0,0)),
-    plot.margin = margin(1,1.5,2,1, 'cm'))
+    plot.margin = margin(1,1.5,2,0.5, 'cm'))
   +ylim(-0.5,0.35)
 )
-p<-p+geom_segment(aes(x=0,xend=0,y=-Inf,yend=0.1975),color='#e5001f',lwd=0.4)+
-  geom_segment(aes(x=0,xend=0,y=-0.1975,yend=0.1975),color='#e5001f',lwd=1.1)
 
 # Preview
 p
